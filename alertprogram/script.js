4c55584ff593643248bd755a6cfeeaa84b3339b9
GantiLakshmiHarika <gantiharika558@gmail.com>
2025-09-05 10:58:11 +0530

    notes

---
+++ alertprogram/script.js
@@ -0,0 +1,8 @@
+console.log("hey hi nenu java script nerchukuntunanu miru start chestara");
+alert("welcome to my website")
+console.log(window)
+ console.log( `iam 
+learning
+from
+youtube 
+javascript`);
