1f6960adcb6b2205be86314ce3a6c8dc0a161cf6
GantiLakshmiHarika <gantiharika558@gmail.com>
2025-09-05 17:28:50 +0530

    notes

---
+++ ammamma/script.js
@@ -0,0 +1,17 @@
+console.log("1 kg onion");
+console.log("1 liter milk");
+// asynchornized process
+setTimeout(function()
+{
+    //synchornized process
+for(let i=0;i<3000000000000;i++)
+{
+    if(i==200000000)
+    {
+     console.log("1 onion dosa");
+    }
+}
+},0);
+console.log("drop chelli at school");
+
+
