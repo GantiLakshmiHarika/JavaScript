5f7a8300cbcd42892681181c5e10b5ae0a1e25e9
GantiLakshmiHarika <gantiharika558@gmail.com>
2025-09-05 16:32:30 +0530

    notes

--- alertprogram/script.js
+++ alertprogram/script.js
@@ -1,8 +1,10 @@
-console.log("hey hi nenu java script nerchukuntunanu miru start chestara");
-alert("welcome to my website")
-console.log(window)
- console.log( `iam 
-learning
-from
-youtube 
-javascript`);
+let ramuamount = 2000;
+let somuamount = 3000;
+let totalamount = ramuamount + somuamount;
+console.log(`ramu's amount`,ramuamount);
+console.log(`somu's amount`,somuamount);
+console.log(`totalamount`,totalamount);
+let marks 
+console.log(marks);
+marks=50;
+console.log(marks);
