7970a3f2a8ff941407032cd57ea5017409a76ffa
GantiLakshmiHarika <gantiharika558@gmail.com>
2025-09-06 11:10:44 +0530

    classes

--- ammamma/script.js
+++ ammamma/script.js
@@ -1,17 +1,32 @@
-console.log("1 kg onion");
-console.log("1 liter milk");
-// asynchornized process
-setTimeout(function()
-{
-    //synchornized process
-for(let i=0;i<3000000000000;i++)
-{
-    if(i==200000000)
+class Book {
+    static author="natural star";
+    constructor(title, price, cost, author) {
+        this.title = title;
+        this.price = price;
+        this.cost = cost;
+    }
+    //object // instance methods 
+    updateprice(newprice)
     {
-     console.log("1 onion dosa");
+        this.price=newprice;
+    }
+    // static methods 
+    static getBookname()
+    {
+     return Book.author;   
     }
 }
-},0);
-console.log("drop chelli at school");
 
-
+let javascript = new Book("JavaScript Latest Edition", 400, 198, "Natural Star ");
+let python = new Book("python Latest Edition", 200, 195, "Natural Star ");
+let cprogramming =new Book("c programming latest edition",350,128,"natural star ");
+let java = new Book(" java latest edition",321,89,"natural star ");
+console.log(javascript.title);
+console.log(python.cost);
+// acessing the static variable withi the class name 
+console.log(Book.author);
+console.log(java.price);
+// updating new price to the exsisting price 
+javascript.updateprice(1009);
+console.log(javascript.price);
+console.log(Book.getBookname());
