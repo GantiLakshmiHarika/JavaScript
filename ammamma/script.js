059806b2e5ccbdb917c048e95e2cf86cfea1665c
GantiLakshmiHarika <gantiharika558@gmail.com>
2025-09-06 18:58:56 +0530

    notes

--- ammamma/script.js
+++ ammamma/script.js
@@ -1,32 +1,42 @@
-class Book {
-    static author="natural star";
-    constructor(title, price, cost, author) {
-        this.title = title;
-        this.price = price;
-        this.cost = cost;
-    }
-    //object // instance methods 
-    updateprice(newprice)
-    {
-        this.price=newprice;
-    }
-    // static methods 
-    static getBookname()
-    {
-     return Book.author;   
-    }
-}
+// class Book {
+//     static author="natural star";
+//     constructor(title, price, cost, author) {
+//         this.title = title;
+//         this.price = price;
+//         this.cost = cost;
+//     }
+//     //object // instance methods 
+//     updateprice(newprice)
+//     {
+//         this.price=newprice;
+//     }
+//     // static methods 
+//     static getBookname()
+//     {
+//      return Book.author;   
+//     }
+// }
 
-let javascript = new Book("JavaScript Latest Edition", 400, 198, "Natural Star ");
-let python = new Book("python Latest Edition", 200, 195, "Natural Star ");
-let cprogramming =new Book("c programming latest edition",350,128,"natural star ");
-let java = new Book(" java latest edition",321,89,"natural star ");
-console.log(javascript.title);
-console.log(python.cost);
-// acessing the static variable withi the class name 
-console.log(Book.author);
-console.log(java.price);
-// updating new price to the exsisting price 
-javascript.updateprice(1009);
-console.log(javascript.price);
-console.log(Book.getBookname());
+// let javascript = new Book("JavaScript Latest Edition", 400, 198, "Natural Star ");
+// let python = new Book("python Latest Edition", 200, 195, "Natural Star ");
+// let cprogramming =new Book("c programming latest edition",350,128,"natural star ");
+// let java = new Book(" java latest edition",321,89,"natural star ");
+// console.log(javascript.title);
+// console.log(python.cost);
+// // acessing the static variable withi the class name 
+// console.log(Book.author);
+// console.log(java.price);
+// // updating new price to the exsisting price 
+// javascript.updateprice(1009);
+// console.log(javascript.price);
+// console.log(Book.getBookname());
+// let harika = "40"; 
+// console.log(Number(harika)); 
+// console.log(+harika);   
+// let sandeep= 2**51;
+// console.log(sandeep);
+// // big int represntation
+// let Sandeep = 2251799813685248;
+// console.log(typeof (BigInt(Sandeep)));
+// let king = 3n;
+// console.log(typeof(king));
