1dec4a00f6230e4d80f2701dc8acb4d088d71e34
GantiLakshmiHarika <gantiharika558@gmail.com>
2025-09-20 22:39:09 +0530

    notes

--- operator/script.js
+++ operator/script.js
@@ -1,4 +1,5 @@
-let num1= 9;
+//Arthmetic operator 
+/* let num1= 9;
 let num2=19;
 console.log("addition:",num1+num2);
 console.log("substraction:",num1-num2);
@@ -8,4 +9,30 @@ console.log("remainder:",num1%num2);
 console.log("exponent:",num1**num2);
 console.log("squareroot:",(num1+num2)**(1/2));
 console.log("squareroot:",Math.sqrt(num1+num2));
-console.log("cuberoot:",Math.cbrt(num1+num2));
+console.log("cuberoot:",Math.cbrt(num1+num2)); */
+
+// Assignment operator 
+/* let value =100;
+console.log("Initial value:", value);
+value+=5;
+console.log(value);
+value-=19;
+console.log(value);
+value*=12;
+console.log(value);
+value**=23;
+console.log(value);
+value%=12;
+console.log(value);
+value/=9;
+console.log(value); */
+
+//Comparison operator
+// let a = 10;
+// let b = 5;
+// console.log(a > b);   
+// console.log(a < b);   
+// console.log(a == 10); 
+// console.log(a === '10'); 
+// console.log(a != b);  
+// console.log(a !== 10);
