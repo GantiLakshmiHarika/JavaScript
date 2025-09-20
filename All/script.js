75fe4e01a21503a1864a68227bf6f8713fd14cb3
GantiLakshmiHarika <gantiharika558@gmail.com>
2025-09-20 12:57:13 +0530

    notes

--- All/script.js
+++ All/script.js
@@ -63,4 +63,56 @@ console.log(dabba);//undefined */
 console.log(dabba); */
 // null it specifes the variables has nothing in it 
 /* let dabba=null;
-console.log(typeof dabba); // typeof null is obje
+console.log(typeof dabba); // typeof null is object */
+/* let username = prompt("what is your name?");
+console.log(username);
+console.log(`welcome to this website ${username}`);
+let height = prompt("enter your height?");
+console.log(height);
+let weight = prompt("enter your weight?");
+console.log(weight);
+let BMI = weight/ ( height ** 2);
+console.log(BMI);
+ */
+/* let marks = 100;
+let firstName = "Suresh";
+let cash = "1000";
+let emptyString = "";
+let isPassed = true;
+let result;
+let myValue = null;
+let bigValue = 500n;
+let s1 = Symbol("id");
+
+console.log("marks:", marks, typeof(marks));
+console.log("firstName:", firstName, typeof(firstName));
+console.log("cash:", cash, typeof(cash));
+console.log("emptyString:", emptyString, typeof(emptyString));
+console.log("isPassed:", isPassed, typeof(isPassed));
+console.log("result:", result, typeof(result));
+console.log("myValue:", myValue, typeof(myValue));
+console.log("bigValue:", bigValue, typeof(bigValue));
+console.log("symbol:", s1, typeof(s1));
+
+
+let markstr = String(marks);
+let firstnames = Number(firstName);
+let cashs = String(cash);
+let emptyStrings = Number(emptyString);
+let isPasses = String(isPassed);
+let results = String(result);
+let myValues = String(myValue);
+let bigValu = String(bigValue);
+let s = String(s1);
+
+
+console.log("markstr:", markstr, typeof(markstr));
+console.log("firstnames:", firstnames, typeof(firstnames));
+console.log("cashs:", cashs, typeof(cashs));
+console.log("emptyStrings:", emptyStrings, typeof(emptyStrings));
+console.log("isPasses:", isPasses, typeof(isPasses));
+console.log("results:", results, typeof(results));
+console.log("myValues:", myValues, typeof(myValues));
+console.log("bigValu:", bigValu, typeof(bigValu));
+console.log("symbol:", s, typeof(s));
+ */
